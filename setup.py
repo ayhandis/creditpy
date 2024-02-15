995e99f3b32c94b8769ac61e06e0127b155ca31f
ayhandis <disayhan@gmail.com>
2024-02-15 17:53:49 +0100

    Update master branch with corrected version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='creditpy',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     author='Ayhan Dis',
     author_email='disayhan@gmail.com',
