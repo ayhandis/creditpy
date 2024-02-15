e3d6155c5c19d5551f9912bd6389806239ca18de
ayhandis <disayhan@gmail.com>
2024-02-15 17:21:17 +0100

    Initial commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='creditpy',
-    version='0.1.0',
+    version='1.0',
     packages=find_packages(),
     author='Ayhan Dis',
     author_email='disayhan@gmail.com',
