37e56b43c32c074f1f3085edd2ebe455bb354477
ayhandis <disayhan@gmail.com>
2024-02-17 00:40:03 +0100

    Your commit message here

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='creditpy',
-    version='1.8',
+    version='2.0',
     packages=find_packages(),
     author='Ayhan Dis',
     author_email='disayhan@gmail.com',
